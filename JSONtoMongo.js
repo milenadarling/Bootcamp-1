6680a2a3fcdea99580f599c26ba0227b35b772a8
Milena <darlingm@ufl.edu>
2019-09-18 16:18:16 -0400

    add

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -38,6 +38,7 @@ fs.readFileSync('listings.json', 'utf8', function(err, data)
   });
 
 });
+process.exit();
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 

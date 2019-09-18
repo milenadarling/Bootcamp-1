299414a8f30e28fc3802fcb9ad8906eeddc4c3be
Milena <darlingm@ufl.edu>
2019-09-18 15:12:26 -0400

    add

--- queries.js
+++ queries.js
@@ -1,5 +1,13 @@
 /* Add all the required libraries*/
 
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
+
+  
+
 /* Connect to your database using mongoose - remember to keep your key secret*/
 
 /* Fill out these functions using Mongoose queries*/
@@ -10,6 +18,11 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({name: 'Library West'}, function(err,data)
+  {
+    if(err) throw err;
+    console.log(data);
+  });
 };
 var removeCable = function() {
   /*
@@ -17,6 +30,10 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+
+   Listing.findOneAndRemove({code: 'CABL'}, function(err,data){
+     if(err) throw err;
+   });
 };
 var updatePhelpsLab = function() {
   /*
@@ -26,11 +43,29 @@ var updatePhelpsLab = function() {
     Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
    */
+  Listing.findOne({name: 'Phelps Laboratory'}, function(err, data)
+    {
+      if(err) throw err;
+
+      data.address = '1953 Museum Rd, Gainesville, FL 32603';
+      data.save(function(err)
+      {
+        if(err) throw err;
+
+        console.log(data);
+      });
+    });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find({},function(err, data)
+  {
+    if(err) throw err;
+    console.log(data);
+  });
+
 };
 
 findLibraryWest();

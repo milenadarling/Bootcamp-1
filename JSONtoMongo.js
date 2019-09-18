38f0842e8f670464210b770589c783aeabcbe236
Milena <darlingm@ufl.edu>
2019-09-18 15:54:03 -0400

    add

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -12,7 +12,7 @@ var fs = require('fs'),
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
-mongoose.connect(config.db.uri,{useNewUrlParser:true});
+mongoose.connect(config.db.uri,{useNewUrlParser:true}, {useUnifiedTopology: true});
 
 var connection = mongoose.connection;
 

3d97af29296ecf8c48280211d4aff88cd923e54c
Milena D <milenamidori5@gmail.com>
2019-09-18 19:11:45 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb://mdarling:test_1234@cluster0-shard-00-00-h7vvs.mongodb.net:27017,cluster0-shard-00-01-h7vvs.mongodb.net:27017,cluster0-shard-00-02-h7vvs.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', //place the URI of your mongo database here.
-  }
-
- 
-};

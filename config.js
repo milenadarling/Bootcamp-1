c92757bb2c98ffb15377c13f814d8068e5e5fe36
Milena <darlingm@ufl.edu>
2019-09-18 11:22:14 -0400

    commiting changes

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: {
+    uri: 'mongodb+srv://mdarling:testtest@cluster0-h7vvs.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+  }
+};

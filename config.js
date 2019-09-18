ac85fd05c076cb4990a7153541163fd65a275037
Milena <darlingm@ufl.edu>
2019-09-18 15:56:12 -0400

    add

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
   db: {
-    uri: 'mongodb://mdarling:testtest@cluster0-shard-00-00-h7vvs.mongodb.net:27017,cluster0-shard-00-01-h7vvs.mongodb.net:27017,cluster0-shard-00-02-h7vvs.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', //place the URI of your mongo database here.
+    uri: 'mongodb+srv://mdarling:testtest@cluster0-h7vvs.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
   }
+
+ 
 };

ee14fd54da17985dba9167c4781ed43d6b19f1d0
Milena <darlingm@ufl.edu>
2019-09-18 17:55:13 -0400

    add

--- queries.js
+++ queries.js
@@ -60,7 +60,7 @@ var updatePhelpsLab = function() {
       if(err) throw err;
 
       data.address = '1953 Museum Rd, Gainesville, FL 32603';
-      Listing.save(function(err)
+      data.save(function(err)
       {
         if(err) throw err;
 
